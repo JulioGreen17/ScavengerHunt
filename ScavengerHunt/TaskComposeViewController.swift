402d8f97a524743ac16f18c23668b05db36069cc
Julio <juliovar123@gmail.com>
2025-03-29 14:09:44 -0700

    Pushing fixes that preivous push reverted

--- ScavengerHunt/TaskComposeViewController.swift
+++ ScavengerHunt/TaskComposeViewController.swift
@@ -16,7 +16,9 @@ class TaskComposeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var titleField: UITextField!
+    
+    @IBOutlet weak var descriptionField: UITextField!
     /*
     // MARK: - Navigation
 
@@ -26,5 +28,15 @@ class TaskComposeViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    // CHecking if it is done
+    @IBAction func didTapDoneButton(_ sender: Any) {
+        guard let title = titleField.text, !title.isEmpty,
+              let description = descriptionField.text, !description.isEmpty else {
+            return}
+        let task = Task(title: title, description: description)
+        onComposeTask?(task)
+        dismiss(animated: true)
+    }
+
 
 }
